159eb31ce470b1ac59eb41e4c07e4bb73a879a9a
Yuuki <e-system@heiseikogyo.co.jp>
2023-11-03 11:01:06 +0900

    コメント機能などを追加しました

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 #PostForm:フォームの名前(ModelFormの一種)
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post    #Postモデルを使う事を伝達
         fields = ('title', 'text',) #フォームではタイトルとテキストのフィールドを使用する
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,18 @@ from django.utils import timezone
 
 #モデル名:Post
 #models.Model:ポストがDjango ModelでDjangoがデータベースに保存すべきものだと分かるように
+#他のモデルへのリンク
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)#他のモデルへのリンク
-    title = models.CharField(max_length=200)#文字数を200文字に制限のフィールド
-    text = models.TextField()#文字制限なしのフィールド
-    created_date = models.DateTimeField(default=timezone.now)#日時と時間のフィールド
-    published_date = models.DateTimeField(blank=True, null=True)#日時と時間のフィールド
+    #著者:ログインユーザー
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #タイトル:文字数を200文字に制限のフィールド
+    title = models.CharField(max_length=200)
+    #文章:文字制限なしのフィールド
+    text = models.TextField()
+    #作成日:日時と時間のフィールド
+    created_date = models.DateTimeField(default=timezone.now)
+    #作成日:日時と時間のフィールド
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +23,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+    
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+    
+    
